91fced2efeae4d918c8650fd3d4935c4f16c6a57
Gabe Gonzales <gq.gonzales1@gmail.com>
2021-06-15 11:11:24 -0500

    Organzing basic structure

---
+++ src/components/Zorber.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import { Route, Redirect } from "react-router-dom";
+// import { ApplicationViews } from "./ApplicationViews";
+// import { NavBar } from "./nav/NavBar";
+// import { Login } from "./auth/Login";
+// import { Register } from "./auth/Register";
+import "./Zorber.css";
+
+export const Zorber = () => (
+  <>
+    <h1>Zorber</h1>
+    <h2>The only app for Zorb enthusiasts</h2>
+  </>
+);
