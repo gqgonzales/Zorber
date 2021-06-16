83b34f70e97af0a971e6a97697785387db23145c
Gabe Gonzales <gq.gonzales1@gmail.com>
2021-06-16 13:58:27 -0500

    Messing with data relationships

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -4,24 +4,27 @@ import { EventList } from "./events/EventList";
 import { NewEventForm } from "./events/NewEventForm";
 import { EventProvider } from "./events/EventProvider";
 import { UserProvider } from "./users/UserProvider";
+import { UserEventsProvider } from "./userEvents/UserEventsProvider";
 
 export const ApplicationViews = () => {
   return (
     <>
       <EventProvider>
         <UserProvider>
-          {/* HOME / UPCOMING */}
-          <Route exact path="/upcoming">
-            <EventList />
-          </Route>
+          <UserEventsProvider>
+            {/* HOME / UPCOMING */}
+            <Route exact path="/upcoming">
+              <EventList />
+            </Route>
 
-          <Route exact path="/create">
-            <NewEventForm />
-          </Route>
+            <Route exact path="/create">
+              <NewEventForm />
+            </Route>
 
-          {/* <Route exact path ="/users">
+            {/* <Route exact path ="/users">
 
           </Route> */}
+          </UserEventsProvider>
         </UserProvider>
       </EventProvider>
     </>
